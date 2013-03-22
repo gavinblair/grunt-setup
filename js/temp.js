b1d60e0e990ad6bd6c75d826a7858a94e2428d0d
gavinblair <gavin.blair@rtraction.com>
2013-03-22 15:16:12 -0400

    init

---
+++ js/temp.js
@@ -0,0 +1,6 @@
+
+var hello_world = 1;
+
+if(hello_world){
+	console.log('hello, world!');
+}
---
+++ js/test/tests.js
@@ -0,0 +1,6 @@
+
+	
+QUnit.test( "hello test", function() {
+  ok( 1 == "1", "QUnit is working!" );
+});
+
